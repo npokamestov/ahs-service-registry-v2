cb53d953c060a61aab03cc6f170e83a6cd6398c4
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-06-28 23:21:18 -0400

    added necessary files for  service registry

---
+++ src/test/java/com/adhocsensei/ahsserviceregistry/AhsServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhocsensei.ahsserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhsServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
